320f29724e9ec24540a1438ca3ef8ffa1f466439
YourAverageSciencePal <40741641+YourAverageSciencePal@users.noreply.github.com>
2020-05-03 22:18:32 +0500

    Dump the classifier. Now does not need retraining every time

--- Prestige/Prestige/mlmodel/ml.py
+++ Prestige/Prestige/mlmodel/ml.py
@@ -1,6 +1,7 @@
 from nltk.corpus import movie_reviews
 from nltk.classify import NaiveBayesClassifier
 from nltk.classify.util import accuracy as nltk_accuracy
+import pickle 
 
 
 def extract_features(words):
@@ -9,28 +10,41 @@ def extract_features(words):
 #import nltk
 #nltk.download('movie_reviews')
 
+
+def save_classifier(classifier):
+   f = open('my_classifier.pickle', 'wb')
+   pickle.dump(classifier, f, -1)
+   f.close()
+   print("Saved the classifier")
+
+def load_classifier():
+   f = open('my_classifier.pickle', 'rb')
+   classifier = pickle.load(f)
+   f.close()
+   return classifier
+
 def SentimentAnalyzer(text):
     # load movie reviews from sample data
-    fileids_pos = movie_reviews.fileids('pos')
-    fileids_neg = movie_reviews.fileids('neg')
+    # fileids_pos = movie_reviews.fileids('pos')
+    # fileids_neg = movie_reviews.fileids('neg')
 
-    features_pos = [(extract_features(movie_reviews.words(fileids=[f])),'Positive') for f in fileids_pos]
-    features_neg = [(extract_features(movie_reviews.words(fileids=[f])),'Negative') for f in fileids_neg]
+    # features_pos = [(extract_features(movie_reviews.words(fileids=[f])),'Positive') for f in fileids_pos]
+    # features_neg = [(extract_features(movie_reviews.words(fileids=[f])),'Negative') for f in fileids_neg]
 
     threshold = 0.8
-    num_pos = int(threshold*len(features_pos))
-    num_neg = int(threshold*len(features_neg))
+    # num_pos = int(threshold*len(features_pos))
+    # num_neg = int(threshold*len(features_neg))
 
-    # creating training and testing data
-    features_train = features_pos[:num_pos] + features_neg[:num_neg]
-    features_test = features_pos[num_pos:] + features_neg[num_neg:]
+    # # creating training and testing data
+    # features_train = features_pos[:num_pos] + features_neg[:num_neg]
+    # features_test = features_pos[num_pos:] + features_neg[num_neg:]
 
     #print('\nNumber of training datapoints:', len(features_train))
     #print('Number of test datapoints:', len(features_test))
 
     # training a naive bayes classifier 
-    classifier = NaiveBayesClassifier.train(features_train)
-    print('Accuracy:',nltk_accuracy(classifier, features_test))
+    classifier = load_classifier()
+    #print('Accuracy:',nltk_accuracy(classifier, features_test))
     results = []
     for comment in text:
         probabilities = classifier.prob_classify(extract_features(comment.split()))
