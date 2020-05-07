babf43ee33aad070e289f7d8953942d9d670f697
hussnainwithss <21100150@lums.edu.pk>
2020-05-07 21:11:44 +0500

    updated settings

--- Prestige/Prestige/Prestige/settings.py
+++ Prestige/Prestige/Prestige/settings.py
@@ -18,7 +18,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -66,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 ]
 
 ROOT_URLCONF = 'Prestige.urls'
