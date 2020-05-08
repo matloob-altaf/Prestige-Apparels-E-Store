575c1ae8b0c4300d154cd37847fbadbadebf15f1
hussnainwithss <21100150@lums.edu.pk>
2020-05-08 12:23:18 +0500

    pushing final

---
+++ Prestige/login_system/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+
+app_name = 'login_system'
+urlpatterns = [
+    path('login/', views.login, name = "login"),
+    path('login/<str:next>', views.login, name = "login"),
+    path('signup/', views.signup, name = "signup"),
+    
+    #path('', views.index, name = "Index"),
+    #path('product/addReview', views.addReview, name = "addReview"),
+    #path('product/<str:slug1>', views.singleProduct, name = "product"),
+    #path('product/', views.singleProduct, name = "product"),
+    #path('catalog/<str:category1>', views.catalog, name = "catalog"),
+    #path('catalog/', views.catalog, name = "catalog"),
+    #path('addReview/<int:id1><user_id1>',views.addReview, name='addReview'),
+    #path('email/',views.addEmail, name='email'),
+    
+    # for search apperance
+    
+    
+    #path('cart/', views.cart, name = "cart"),
+    #path('technology/', views.technology, name = "technology"),
+    #path('about/', views.about, name = "about"),
+    #path('contact/', views.contact, name = "contact"),
+    
+
+]
